25113ad956e24e2a3b6719656ce330866aac2d4e
wangboning <meiniepan@163.com>
2018-08-22 18:19:23 +0800

    fix 压缩尺寸

--- app/src/main/java/com/example/meiniepan/pic2ascii/CommonUtil.java
+++ app/src/main/java/com/example/meiniepan/pic2ascii/CommonUtil.java
@@ -3,10 +3,13 @@ package com.example.meiniepan.pic2ascii;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.PorterDuff;
 import android.graphics.Typeface;
+import android.media.ExifInterface;
 import android.os.Environment;
 import android.text.Layout;
 import android.text.StaticLayout;
@@ -28,6 +31,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.UUID;
 
 /**
@@ -105,7 +111,7 @@ public class CommonUtil {
             bmp.compress(Bitmap.CompressFormat.JPEG, 100, fos);
             fos.flush();
             fos.close();
-            Toast.makeText(context,"保存成功,位置:"+filePic.getAbsolutePath(),Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "保存成功,位置:" + filePic.getAbsolutePath(), Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -129,7 +135,7 @@ public class CommonUtil {
 
     }
 
-    public static Bitmap createAsciiPic(final Bitmap bitmap, Context context) {
+    public static Bitmap createAsciiPic(final String path, Context context) {
         final String base = "#8XOHLTI)i=+;:,.";// 字符串由复杂到简单
 //        final String base = "#,.0123456789:;@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";// 字符串由复杂到简单
         StringBuilder text = new StringBuilder();
@@ -137,9 +143,20 @@ public class CommonUtil {
         DisplayMetrics dm = new DisplayMetrics();
         wm.getDefaultDisplay().getMetrics(dm);
         int width = dm.widthPixels;
-        int heitht = dm.heightPixels;
-        final Bitmap image = scale(bitmap, width / 3, heitht / 3);  //读取图片
-//        final Bitmap image = bitmap;  //读取图片
+        int height = dm.heightPixels;
+         Bitmap image = BitmapFactory.decodeFile(path);  //读取图片
+        int width0 = image.getWidth();
+        int height0 = image.getHeight();
+        int width1,height1;
+        int scale = 3;
+        if (width0<= width/scale){
+            width1 = width0;
+            height1 = height0;
+        }else {
+            width1 = width/scale;
+            height1 = width1*height0/width0;
+        }
+          image = scale(path, width1, height1);  //读取图片
         //输出到指定文件中
         for (int y = 0; y < image.getHeight(); y += 2) {
             for (int x = 0; x < image.getWidth(); x++) {
@@ -152,8 +169,8 @@ public class CommonUtil {
             }
             text.append("\n");
         }
-//        return creatCodeBitmap(text, context);
         return textAsBitmap(text, context);
+//        return image;
     }
 
     public static Bitmap creatCodeBitmap(StringBuilder contents, Context context) {
@@ -194,7 +211,7 @@ public class CommonUtil {
         textPaint.setAntiAlias(true);
         textPaint.setTypeface(Typeface.MONOSPACE);
 
-        textPaint.setTextSize(5);
+        textPaint.setTextSize(12);
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics dm = new DisplayMetrics();
         wm.getDefaultDisplay().getMetrics(dm);
@@ -202,7 +219,7 @@ public class CommonUtil {
 
         StaticLayout layout = new StaticLayout(text, textPaint, width,
 
-                Layout.Alignment.ALIGN_NORMAL, 1f, 0.0f, true);
+                Layout.Alignment.ALIGN_CENTER, 1f, 0.0f, true);
 
         Bitmap bitmap = Bitmap.createBitmap(layout.getWidth() + 20,
 
@@ -226,8 +243,207 @@ public class CommonUtil {
 
     }
 
-    public static Bitmap scale(Bitmap src, int newWidth, int newHeight) {
-        Bitmap ret = Bitmap.createScaledBitmap(src, newWidth, newHeight, true);
+    public static Bitmap scale(String src, int newWidth, int newHeight) {
+        Bitmap ret = Bitmap.createScaledBitmap(BitmapFactory.decodeFile(src), newWidth, newHeight, true);
         return ret;
+    }
+
+    /**
+     * 存放拍摄图片的文件夹
+     */
+    private static final String FILES_NAME = "/MyPhoto";
+    /**
+     * 获取的时间格式
+     */
+    public static final String TIME_STYLE = "yyyyMMddHHmmss";
+    /**
+     * 图片种类
+     */
+    public static final String IMAGE_TYPE = ".png";
+
+
+    /**
+     * 获取手机可存储路径
+     *
+     * @param context 上下文
+     * @return 手机可存储路径
+     */
+    private static String getPhoneRootPath(Context context) {
+        // 是否有SD卡
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)
+                || !Environment.isExternalStorageRemovable()) {
+            // 获取SD卡根目录
+            return context.getExternalCacheDir().getPath();
+        } else {
+            // 获取apk包下的缓存路径
+            return context.getCacheDir().getPath();
+        }
+    }
+
+    /**
+     * 使用当前系统时间作为上传图片的名称
+     *
+     * @return 存储的根路径+图片名称
+     */
+    public static String getPhotoFileName(Context context) {
+        File file = new File(getPhoneRootPath(context) + FILES_NAME);
+        // 判断文件是否已经存在，不存在则创建
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        // 设置图片文件名称
+        SimpleDateFormat format = new SimpleDateFormat(TIME_STYLE, Locale.getDefault());
+        Date date = new Date(System.currentTimeMillis());
+        String time = format.format(date);
+        String photoName = "/" + time + IMAGE_TYPE;
+        return file + photoName;
+    }
+
+    /**
+     * 保存Bitmap图片在SD卡中
+     * 如果没有SD卡则存在手机中
+     *
+     * @param mbitmap 需要保存的Bitmap图片
+     * @return 保存成功时返回图片的路径，失败时返回null
+     */
+    public static String savePhotoToSD(Bitmap mbitmap, Context context) {
+        FileOutputStream outStream = null;
+        String fileName = getPhotoFileName(context);
+        try {
+            outStream = new FileOutputStream(fileName);
+            // 把数据写入文件，100表示不压缩
+            mbitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+            return fileName;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if (outStream != null) {
+                    // 记得要关闭流！
+                    outStream.close();
+                }
+                if (mbitmap != null) {
+                    mbitmap.recycle();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 把原图按1/10的比例压缩
+     *
+     * @param path 原图的路径
+     * @return 压缩后的图片
+     */
+    public static Bitmap getCompressPhoto(String path) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = false;
+        options.inSampleSize = 10; // 图片的大小设置为原来的十分之一
+        Bitmap bmp = BitmapFactory.decodeFile(path, options);
+        options = null;
+        return bmp;
+    }
+
+    /**
+     * 处理旋转后的图片
+     *
+     * @param originpath 原图路径
+     * @param context    上下文
+     * @return 返回修复完毕后的图片路径
+     */
+    public static String amendRotatePhoto(String originpath, Context context) {
+
+        // 取得图片旋转角度
+        int angle = readPictureDegree(originpath);
+
+        // 把原图压缩后得到Bitmap对象
+        if (angle != 0) {
+            Bitmap bmp = getCompressPhoto(originpath);
+            Bitmap bitmap = rotaingImageView(angle, bmp);
+            return savePhotoToSD(bitmap, context);
+        } else {
+            return originpath;
+        }
+
+        // 修复图片被旋转的角度
+
+        // 保存修复后的图片并返回保存后的图片路径
+    }
+
+    /**
+     * 读取照片旋转角度
+     *
+     * @param path 照片路径
+     * @return 角度
+     */
+    public static int readPictureDegree(String path) {
+        int degree = 0;
+        try {
+            ExifInterface exifInterface = new ExifInterface(path);
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return degree;
+    }
+
+    /**
+     * 旋转图片
+     *
+     * @param angle  被旋转角度
+     * @param bitmap 图片对象
+     * @return 旋转后的图片
+     */
+    public static Bitmap rotaingImageView(int angle, Bitmap bitmap) {
+        Bitmap returnBm = null;
+        // 根据旋转角度，生成旋转矩阵
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        try {
+            // 将原始图片按照旋转矩阵进行旋转，并得到新的图片
+            returnBm = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        } catch (OutOfMemoryError e) {
+        }
+        if (returnBm == null) {
+            returnBm = bitmap;
+        }
+        if (bitmap != returnBm) {
+            bitmap.recycle();
+        }
+        return returnBm;
+    }
+
+    public static Bitmap sizeCompres(String path, int rqsW, int rqsH) {
+        // 用option设置返回的bitmap对象的一些属性参数
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;// 设置仅读取Bitmap的宽高而不读取内容
+        BitmapFactory.decodeFile(path, options);// 获取到图片的宽高，放在option里边
+        final int height = options.outHeight;//图片的高度放在option里的outHeight属性中
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+        if (rqsW == 0 || rqsH == 0) {
+            options.inSampleSize = 1;
+        } else if (height > rqsH || width > rqsW) {
+            final int heightRatio = Math.round((float) height / (float) rqsH);
+            final int widthRatio = Math.round((float) width / (float) rqsW);
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+            options.inSampleSize = inSampleSize;
+            options.inJustDecodeBounds = false;
+        }
+        return BitmapFactory.decodeFile(path, options);// 主要通过option里的inSampleSize对原图片进行按比例压缩
     }
 }
--- app/src/main/java/com/example/meiniepan/pic2ascii/MainActivity.java
+++ app/src/main/java/com/example/meiniepan/pic2ascii/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -19,6 +20,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     private Bitmap bitmap;
+    private String filepath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,17 +50,18 @@ public class MainActivity extends AppCompatActivity {
                         path = localMedia.getPath();
                     }
                 }
-                bitmap = BitmapFactory.decodeFile(path);
-                imageView.setImageBitmap(CommonUtil.createAsciiPic(bitmap, MainActivity.this));
+                 filepath = CommonUtil.amendRotatePhoto(path, MainActivity.this);
+//                imageView.setImageBitmap(BitmapFactory.decodeFile(filepath));
+                imageView.setImageBitmap(CommonUtil.createAsciiPic(filepath, MainActivity.this));
             }
         }
     }
 
     public void doSave(View view) {
-        if (bitmap == null){
+        if (TextUtils.isEmpty(filepath)){
             Toast.makeText(MainActivity.this,"请先生成图片",Toast.LENGTH_SHORT).show();
             return;
         }
-                CommonUtil.saveBitmap2file(CommonUtil.createAsciiPic(bitmap, MainActivity.this),System.currentTimeMillis()+"",MainActivity.this);
+                CommonUtil.saveBitmap2file(CommonUtil.createAsciiPic(filepath, MainActivity.this),System.currentTimeMillis()+"",MainActivity.this);
     }
 }
